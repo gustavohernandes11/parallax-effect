765fdf6a547e81a6b679dca83e19c78b9110ec51
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-08-21 20:24:49 -0300

    feat: adição de componentes básicos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 import type { NextPage } from "next";
+import { Section } from "components/Section";
+import { Heading } from "components/Heading";
+import { Header } from "components/Header";
 
 const Home: NextPage = () => {
-
     return (
-           <>
-           <h1>Change-me</h1>
-           </>
+        <>
+            <Header />
+            <Section id="section-1" full={true}>
+                <Heading>Section 1</Heading>
+            </Section>
+            <Section id="section-2" full={true}>
+                <Heading>Section 2</Heading>
+            </Section>
+        </>
     );
 };
 
 export default Home;
-
